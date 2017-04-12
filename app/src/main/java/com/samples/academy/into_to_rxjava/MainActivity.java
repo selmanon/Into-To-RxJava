d28356c271eca137acc729cf9bf1a865be5a2576
selmanon <selassi.abdellah@gmail.com>
2017-04-12 15:44:32 +0100

    Initial commit

---
+++ app/src/main/java/com/samples/academy/into_to_rxjava/MainActivity.java
@@ -0,0 +1,18 @@
+package com.samples.academy.into_to_rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/samples/academy/into_to_rxjava/ExampleUnitTest.java
@@ -0,0 +1,82 @@
+package com.samples.academy.into_to_rxjava;
+
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.observables.ConnectableObservable;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void cold() throws InterruptedException {
+        Observable<Long> cold = Observable.interval(200, TimeUnit.MILLISECONDS).map(i -> i * 2);
+
+        cold.subscribe(i -> System.out.println("First: " + i));
+        Thread.sleep(500);
+        cold.subscribe(i -> System.out.println("Second: " + i));
+    }
+
+    @Test
+    public void connect() throws InterruptedException {
+        ConnectableObservable<Long> cold = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
+
+        cold.connect();
+
+        cold.subscribe(i -> System.out.println("First: " + i));
+        cold.subscribe(i -> System.out.println("Second: " + i));
+        Thread.sleep(1500);
+    }
+
+    @Test
+    public void disconnect() throws InterruptedException {
+        ConnectableObservable<Long> connectable = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
+        Subscription s = connectable.connect();
+
+        connectable.subscribe(i -> System.out.println(i));
+
+        Thread.sleep(1000);
+        System.out.println("Closing connection");
+        s.unsubscribe();
+
+        System.out.println("Reconnecting");
+        s = connectable.connect();
+    }
+
+    @Test
+    public void refCount() throws InterruptedException {
+        Observable<Long> cold = Observable.interval(200, TimeUnit.MILLISECONDS).publish().refCount();
+
+        Subscription s1 = cold.subscribe(i -> System.out.println("First: " + i));
+        Thread.sleep(500);
+
+        Subscription s2 = cold.subscribe(i -> System.out.println("Second: " + i));
+        Thread.sleep(500);
+
+        System.out.println("Unsubscribe second");
+        s2.unsubscribe();
+
+        Thread.sleep(500);
+        System.out.println("Unsubscribe first");
+        s1.unsubscribe();
+
+        System.out.println("First connection again");
+        Thread.sleep(500);
+        s1 = cold.subscribe(i -> System.out.println("First: " + i));
+        Thread.sleep(500);
+    }
+
+}
